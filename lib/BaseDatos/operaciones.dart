6ad6a64919455477a465ed0116bef5b21eec6518
BomayeEmiliano <emi_bore.r@hotmail.com>
2024-07-22 16:33:58 -0600

    Insertar datos a la Tabla

--- lib/BaseDatos/operaciones.dart
+++ lib/BaseDatos/operaciones.dart
@@ -1,3 +1,4 @@
+import 'package:appcompleto/BaseDatos/nota.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 
@@ -9,13 +10,17 @@ class Operaciones {
 
     return openDatabase(join(await getDatabasesPath(),'appdb.db'),
             onCreate: (db, version) {
-              return db.execute("CREATE TABLE IF NOT EXISTS appdb (id INTEGER PRIMARY KEY, nombre TEXT, apellido TEXT, correo TEXT, contrasena TEXT, confcontrasena TEXT)");
+              return db.execute("CREATE TABLE IF NOT EXISTS usuarios (id INTEGER PRIMARY KEY, nombre TEXT, apellido TEXT, correo TEXT, contrasena TEXT, confcontrasena TEXT)");
             }, version: 1,
     
     );
 
   }
 
+// Insertar Elemento a la Tabla 
+ Future<void> _isertarAppbd(Nota nota) async{
+  Database database=await _abrirBD();
+  database.insert("usuarios", nota.toMap());
+ }
 
-  
 }
